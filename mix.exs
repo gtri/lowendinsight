5160bc784f0b52911c8474695f679677c8119de0
kitplummer <kit.plummer@gtri.gatech.edu>
2019-08-16 13:07:19 -0400

    add: git client dependency

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule GithubModule.MixProject do
   defp deps do
     [
       {:tentacat, "~> 1.0"},
+      {:git_cli, "~> 0.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
