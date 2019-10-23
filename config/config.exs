0fe88dc5ab3c044817490cd5a3cb32980e5dc11c
kitplummer <kit.plummer@gtri.gatech.edu>
2019-10-23 12:17:38 -0400

    fix: token expired

--- config/config.exs
+++ config/config.exs
@@ -5,7 +5,7 @@
 use Mix.Config
 
 config :lowendinsight,
-  access_token: "2985bf2c1ff4b41863aef325b2ed527a120b6bc0",
+  access_token: "",
   critical_contributor_par_level: 2,
   high_contributor_par_level: 3,
   medium_contributor_par_level: 5,
