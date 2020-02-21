9fcbed727bede2303a7391e5e229295d6fca8388
Kit Plummer <kitplummer@gmail.com>
2020-02-21 08:00:51 -0500

    fix: no async on project ident tests

--- test/project_ident_test.exs
+++ test/project_ident_test.exs
@@ -3,7 +3,7 @@
 # the BSD 3-Clause license. See the LICENSE file for details.
 
 defmodule ProjectIdentTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   doctest ProjectIdent
   test "is_mix?(repo)" do
@@ -27,32 +27,42 @@ defmodule ProjectIdentTest do
   end
 
   test "is_go_mod?(repo)" do
+    {:ok, cwd} = File.cwd
     {:ok, repo} = GitModule.clone_repo("https://github.com/go-kit/kit")
-    assert true == ProjectIdent.is_go_mod?(repo)
+    assert %{"go_mod" => ["#{cwd}/kit/go.mod"]} == ProjectIdent.is_go_mod?(repo)
     GitModule.delete_repo(repo)
   end
 
   test "is_cargo?(repo)" do
+    {:ok, cwd} = File.cwd
     {:ok, repo} = GitModule.clone_repo("https://github.com/clap-rs/clap")
-    assert true == ProjectIdent.is_cargo?(repo)
+    assert %{"cargo" => ["#{cwd}/clap/Cargo.toml"]} == ProjectIdent.is_cargo?(repo)
     GitModule.delete_repo(repo)
   end
 
   test "is_rubygem?(repo)" do
+    {:ok, cwd} = File.cwd
     {:ok, repo} = GitModule.clone_repo("https://github.com/rubocop-hq/rubocop")
-    assert true == ProjectIdent.is_rubygem?(repo)
+    assert %{"rubygem" => ["#{cwd}/rubocop/Gemfile"]} == ProjectIdent.is_rubygem?(repo)
     GitModule.delete_repo(repo)
   end
 
   test "is_maven?(repo)" do
+    {:ok, cwd} = File.cwd
     {:ok, repo} = GitModule.clone_repo("https://github.com/snyk/snyk-maven-plugin")
-    assert true == ProjectIdent.is_maven?(repo)
+    assert %{"maven" => ["#{cwd}/snyk-maven-plugin/pom.xml",
+                         "#{cwd}/snyk-maven-plugin/src/it/license-issues-module/pom.xml",
+                         "#{cwd}/snyk-maven-plugin/src/it/multi-module/child-module/pom.xml",
+                         "#{cwd}/snyk-maven-plugin/src/it/multi-module/pom.xml",
+                         "#{cwd}/snyk-maven-plugin/src/it/private-repo-module/pom.xml",
+                         "#{cwd}/snyk-maven-plugin/src/it/single-module/pom.xml"]} == ProjectIdent.is_maven?(repo)
     GitModule.delete_repo(repo)
   end
 
   test "is_gradle?(repo)" do
+    {:ok, cwd} = File.cwd
     {:ok, repo} = GitModule.clone_repo("https://github.com/ReactiveX/RxKotlin")
-    assert true == ProjectIdent.is_gradle?(repo)
+    assert %{"gradle" => ["#{cwd}/RxKotlin/build.gradle.kts"]} == ProjectIdent.is_gradle?(repo)
     GitModule.delete_repo(repo)
   end
 
