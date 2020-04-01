b16c37b91b7103e4d2dddf28b60dd45b09c7ff09
kitplummer <kit.plummer@gtri.gatech.edu>
2020-04-01 17:54:39 -0400

    feat: move version to 0.5.0

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule GithubModule.MixProject do
     [
       app: :lowendinsight,
       description: description(),
-      version: "0.4.5",
+      version: "0.5.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
