cc410ccacfd88303f90ae825d0cfcec2451258e6
haldihri3 <houida.aldihri@gtri.gatech.edu>
2020-07-27 15:04:06 -0400

    fix: typo in scheme check

--- lib/analyzer_module.ex
+++ lib/analyzer_module.ex
@@ -47,7 +47,7 @@ defmodule AnalyzerModule do
           uri.scheme == "file" ->
             GitModule.get_repo(uri.path)
 
-          uri.scheme == "https" or uri.scheme == "http" or uri.scheme === "git+https"->
+          uri.scheme == "https" or uri.scheme == "http" or uri.scheme == "git+https" ->
             
             url = Helpers.remove_git_prefix(url)
 
