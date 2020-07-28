3cd8d69bb6c3783dd6b2c7245b8d7b518546ed99
haldihri3 <houida.aldihri@gtri.gatech.edu>
2020-07-27 22:27:41 -0400

    format: removed extra spaces

--- lib/npm/packagefile.ex
+++ lib/npm/packagefile.ex
@@ -1,15 +1,14 @@
 defmodule Npm.Packagefile do
+  @spec parse!(binary) :: {[any], non_neg_integer}
+  def parse!(content) do
+    deps =
+      content
+      |> Jason.decode()
+      |> extract_deps
+      |> Enum.to_list()
 
-    @spec parse!(binary) :: {[any], non_neg_integer}
-    def parse!(content) do
-        deps =
-            content
-            |> Jason.decode
-            |> extract_deps
-            |> Enum.to_list
-        
-        {deps, length(deps)}
-    end
+    {deps, length(deps)}
+  end
 
-    defp extract_deps({:ok, %{"dependencies" => deps}}), do: deps
+  defp extract_deps({:ok, %{"dependencies" => deps}}), do: deps
 end
