815484fd6b5cc9542dc0526deb4fd4a14695e393
haldihri3 <houida.aldihri@gtri.gatech.edu>
2020-07-27 22:28:11 -0400

    Parser returns dependency and version

--- lib/npm/packagelockfile.ex
+++ lib/npm/packagelockfile.ex
@@ -1,15 +1,15 @@
 defmodule Npm.Packagelockfile do
+  @spec parse!(binary) :: {[any], non_neg_integer}
+  def parse!(content) do
+    deps =
+      content
+      |> Jason.decode()
+      |> extract_deps
+      |> Enum.to_list()
+      |> Enum.map(fn {dependency, info} -> {dependency, info["version"]} end)
 
-    @spec parse!(binary) :: {[any], non_neg_integer}
-    def parse!(content) do
-        deps =
-            content
-            |> Jason.decode
-            |> extract_deps
-            |> Enum.to_list
-        
-        {deps, length(deps)}
-    end
+    {deps, length(deps)}
+  end
 
-    defp extract_deps({:ok, %{"dependencies" => deps}}), do: deps
+  defp extract_deps({:ok, %{"dependencies" => deps}}), do: deps
 end
