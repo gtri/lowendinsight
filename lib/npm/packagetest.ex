a7463902f67f082dd153425cd56159bad5ac4965
Taylor Wentzel <thwentzel98@gmail.com>
2020-07-29 15:06:45 -0400

    make entrypoint exec

--- lib/npm/packagetest.ex
+++
@@ -1,9 +0,0 @@
-#defmodule Packagetest do
-#    def print_json() do
-#        File.read!("lib/npm/package-lock.json")
-#        |> Packagelockfile.parse!()
-#        |> Enum.map(fn package ->
-#            ScannerModule.query_npm(package)
-#      end)
-#    end
-#end
