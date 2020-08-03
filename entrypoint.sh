e254fe146a295b4295bf166824e8db0cd190e532
Taylor Wentzel <thwentzel98@gmail.com>
2020-08-03 17:36:27 -0400

    adding ability to generate .json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,39 @@
 #!/bin/bash -l
+set -e
 
-cd /opt/app
-mix local.hex --force
-mix lei.scan ${GITHUB_WORKSPACE}
+#cd /opt/app
+#mix local.hex --force
+#mix lei.scan ${GITHUB_WORKSPACE}
+
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+touch foo.json
+git add foo.json
+git commit -m "Add changes" -a
+
+INPUT_BRANCH=${INPUT_BRANCH:-master}
+INPUT_FORCE=${INPUT_FORCE:-false}
+INPUT_TAGS=${INPUT_TAGS:-false}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+_FORCE_OPTION=''
+REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
+
+echo "Push to branch $INPUT_BRANCH";
+[ -z "${INPUT_GITHUB_TOKEN}" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+if ${INPUT_FORCE}; then
+    _FORCE_OPTION='--force'
+fi
+
+if ${INPUT_TAGS}; then
+    _TAGS='--tags'
+fi
+
+cd ${INPUT_DIRECTORY}
+
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
