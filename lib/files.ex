4b8b364384944fcb6486444c66c8bb7a2531034a
Kit Plummer <kitplummer@gmail.com>
2022-12-26 17:04:30 -0500

    init: files insight

---
+++ lib/files.ex
@@ -0,0 +1,71 @@
+# Copyright (C) 2022 by Kit Plummer
+# This software may be modified and distributed under the terms of
+# the BSD 3-Clause license. See the LICENSE file for details.
+
+defmodule Lowendinsight.Files do
+
+  @spec analyze_files(binary) :: %{
+          binary_files: list,
+          binary_files_count: non_neg_integer,
+          has_contributing: boolean,
+          has_license: boolean,
+          has_readme: boolean,
+          total_file_count: non_neg_integer
+        }
+  def analyze_files(path) do
+    binaries = find_binary_files(path)
+    res = Map.merge(binaries, get_total_file_count(path))
+    res = Map.merge(res, has_readme?(path))
+    res = Map.merge(res, has_license?(path))
+    Map.merge(res, has_contributing?(path))
+  end
+
+  @spec find_binary_files(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            )
+        ) :: %{binary_files: list, binary_files_count: non_neg_integer}
+  def find_binary_files(path) do
+    binary_files =
+      File.cd!(path, fn ->
+        System.cmd("grep", ["-rIL", "."])
+      end)
+      |> elem(0)
+      |> String.split("\n")
+      |> Enum.reject(& String.contains?(&1, ".git/")|| &1 == "")
+
+    %{binary_files: binary_files, binary_files_count: Enum.count(binary_files)}
+  end
+
+  @spec get_total_file_count(binary) :: %{total_file_count: non_neg_integer}
+  def get_total_file_count(path) do
+    all_files =
+      Path.wildcard(path <> "/**")
+      |> Enum.reject(& String.contains?(&1, ".git/")|| &1 == "")
+    total_file_count = Enum.count(all_files)
+    %{total_file_count: total_file_count}
+  end
+
+  @spec has_readme?(binary) :: %{has_readme: boolean}
+  def has_readme?(path) do
+    readmes =
+      Path.wildcard(path <> "/readme*") ++ Path.wildcard(path <> "/README*")
+    %{has_readme: !Enum.empty?(readmes)}
+  end
+
+  @spec has_license?(binary) :: %{has_license: boolean}
+  def has_license?(path) do
+    licenses =
+      Path.wildcard(path <> "/license*") ++ Path.wildcard(path <> "/LICENSE*")
+    %{has_license: !Enum.empty?(licenses)}
+  end
+
+  @spec has_contributing?(binary) :: %{has_contributing: boolean}
+  def has_contributing?(path) do
+    contributings =
+      Path.wildcard(path <> "/contributing*") ++ Path.wildcard(path <> "/CONTRIBUTING*")
+    %{has_contributing: !Enum.empty?(contributings)}
+  end
+end
---
+++ test/files_test.exs
@@ -0,0 +1,31 @@
+# Copyright (C) 2022 by Kit Plummer
+# This software may be modified and distributed under the terms of
+# the BSD 3-Clause license. See the LICENSE file for details.
+
+defmodule FilesTest do
+  use ExUnit.Case
+  doctest Lowendinsight.Files
+  # setup_all do
+  #   {:ok, tmp_path} = Temp.path("lei-analyzer-test")
+  #   {:ok, repo} = GitModule.clone_repo("https://github.com/kitplummer/xmpp4rails", tmp_path)
+  #   {:ok, date} = GitModule.get_last_commit_date(repo)
+  #   GitModule.delete_repo(repo)
+  #   weeks = TimeHelper.get_commit_delta(date) |> TimeHelper.sec_to_weeks()
+  #   [weeks: weeks]
+  # end
+
+  test "analyze files in path repo" do
+    {:ok, report} =
+      AnalyzerModule.analyze(["https://github.com/kitplummer/xmpp4rails"], "files_path_test", DateTime.utc_now(), %{types: false})
+
+    assert "complete" == report[:state]
+    repo_data = List.first(report[:report][:repos])
+    assert "files_path_test" == repo_data[:header][:source_client]
+    assert %{binary_files: [],
+             binary_files_count: 0,
+             total_file_count: 15,
+             has_readme: true,
+             has_license: true,
+             has_contributing: false} == repo_data[:data][:files]
+  end
+end
