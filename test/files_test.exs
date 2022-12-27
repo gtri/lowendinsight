4ab2b29594d824d2003136d8bb5be49e4425753c
Kit Plummer <kitplummer@gmail.com>
2022-12-27 15:44:03 -0500

    fix test again

--- test/files_test.exs
+++ test/files_test.exs
@@ -36,8 +36,8 @@ defmodule FilesTest do
     repo_data = List.first(report[:report][:repos])
     assert "files_path_test" == repo_data[:header][:source_client]
     assert %{binary_files: ["lei_bus_128.png"],
-             binary_files_count: 2,
-             total_file_count: 176,
+             binary_files_count: 1,
+             total_file_count: 178,
              has_readme: true,
              has_license: true,
              has_contributing: false} == repo_data[:data][:files]
